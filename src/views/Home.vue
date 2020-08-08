ff6d922a483d8107b1043357a9e6e856287b1c9c
Binoculus <929477145@qq.com>
2020-08-08 22:37:36 +0800

    start

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header title="标题"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
